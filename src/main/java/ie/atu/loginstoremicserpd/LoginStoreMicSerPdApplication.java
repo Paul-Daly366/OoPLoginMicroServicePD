fe3ff41ed7eb97fec03c6e08b5062d3e32a5b6ee
PaulDaly-STUDENT <g00470372@atu.ie>
2026-04-13 10:48:39 +0100

    Initial

---
+++ src/main/java/ie/atu/loginstoremicserpd/LoginStoreMicSerPdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.loginstoremicserpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginStoreMicSerPdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginStoreMicSerPdApplication.class, args);
+    }
+
+}
